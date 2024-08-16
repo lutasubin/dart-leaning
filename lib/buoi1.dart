491aa44d258207b745acd5778fa5aca327cd9017
lutasubin <ducthanh22112004@gmail.com>
2024-08-16 21:26:59 +0700

    dart

---
+++ lib/buoi1.dart
@@ -0,0 +1,36 @@
+void main(){
+  var myName ='huy 2';
+  print('thanh nguyen');
+
+  var age =2;
+
+  age =25;//phai dung so nguyen
+  // String name ="nguyen duc thanh";
+
+  // bool isStudent =true;
+
+  // name ='lo van son';
+  print('gia tri cua age la:$age');
+  print('gia tri cua age la $age');
+
+final year =2014;
+
+// year=2025;     khong cho  gans lai
+  const month=0;
+
+  // month=10; khong dc gan
+//dynamic
+dynamic a= 'iii';
+print('gia tri cua a la:$a');
+a=1;
+
+print('gia tri cua a sau khi  thay doi la $a');
+//int,double,string,bool,list
+
+//bien 1 cachs luu tru va quan ly du lieu trong chuong trinh 
+
+//ban co the khai bao bien voi chi  dinh hoac khong can chi dinh kieu dl
+// vd final,const khai bao bien khong thay doi|gan duy nhat
+
+
+}
